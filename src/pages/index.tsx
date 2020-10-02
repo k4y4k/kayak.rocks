483170f9fcb81119049d78aabdbcf52ec61e4d38
kayak <kayak@kayak.rocks>
2020-10-02 16:45:19 +1000

    feat: use tailwindcss via styled-components; move to yarn
    - Move to Yarn
    - Add styled-components, twin.macro and tailwindCSS
    - Add empty config file for tailwindCSS
    - Add type declarations for styled-components
    
    The move to Yarn shouldn't break anything. SHOULDN'T.
    
    closes #5, closes #6, and closes #8

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { PageProps } from 'gatsby';
+import React from 'react'
+import { PageProps } from 'gatsby'
 
-import Title from '@/components/Title';
-import Button from '@/components/layout/Button';
+import Title from '@/components/Title'
+import Button from '@/components/layout/Button'
 
 const Home: React.FC<PageProps> = () => (
   <main>
@@ -10,6 +10,6 @@ const Home: React.FC<PageProps> = () => (
     <p>My name is kayak and this is TypeScript, ooh I&apos;m a big girl now</p>
     <Button>This is a button.</Button>
   </main>
-);
+)
 
-export default Home;
+export default Home
