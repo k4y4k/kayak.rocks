bb7b71b3de44a10558a01120ee7adc0057abe878
kayak <kayak@kayak.rocks>
2020-10-03 16:12:51 +1000

    feat(about): add about section
    closes #26

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { PageProps } from 'gatsby'
-
 import Hero from '@/components/Hero/HeroContainer'
 import Work from '@/components/Work/WorkSection'
+import About from '@/components/About/AboutSection'
 
 const Home: React.FC<PageProps> = () => (
   <main>
     <Hero />
     <Work />
+    <About />
   </main>
 )
 
