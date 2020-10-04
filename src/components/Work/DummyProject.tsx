d61f1f8a991856fb74c83e02006fd2554e0803e4
kayak <kayak@kayak.rocks>
2020-10-04 13:05:24 +1000

    feat(projects): redesign project cards
    closes #47

--- src/components/Work/DummyProject.tsx
+++ src/components/Work/DummyProject.tsx
@@ -2,8 +2,7 @@ import React from 'react'
 import tw, { styled } from 'twin.macro'
 
 const Project = styled.article`
-  max-width: calc(900px / 3);
-  ${tw`uppercase border p-2 m-2`}
+  ${tw`bg-dodgerblue-900 text-white p-2 m-2 flex`}
 `
 
 const Tag = styled.span`
@@ -11,13 +10,39 @@ const Tag = styled.span`
   font-family: "Inconsolata", monospace;
 `
 
+const ProjectTitle = styled.h1`
+  ${tw`uppercase text-2xl font-black`}
+  font-family: "Inconsolata", monospace
+`
+
+const ProjectImg = styled.div`
+  ${tw`bg-gray-500`}
+  width: calc(100vw * 0.6);
+  height: calc(100vh * 0.6);
+`
+
+const ProjectDetailsContainer = styled.div`
+  ${tw`flex flex-col p-4`}
+`
+
 const DummyProject: React.FC = () => (
   <Project>
-    DummyProject
-    <br />
-    {['JS', 'React', 'Gatsby'].map(tech => (
-      <Tag>{tech}</Tag>
-    ))}
+    <ProjectImg />
+
+    <ProjectDetailsContainer>
+      <ProjectTitle>Dummy Project</ProjectTitle>
+
+      <p>
+        Aut autem molestiae dicta est qui culpa eos omnis. Cum rerum sunt
+        exercitationem. Autem numquam est qui voluptatem a molestias sit et.
+        Optio ullam a praesentium eos sed accusantium. Quo cumque eos aut error
+        facere perspiciatis voluptas itaque. Voluptatem ex et molestiae aliquam.
+      </p>
+
+      {['Gatsby', 'React', 'TypeScript'].map(tech => (
+        <Tag key={tech}>{tech}</Tag>
+      ))}
+    </ProjectDetailsContainer>
   </Project>
 )
 
--- src/components/Work/WorkSection.tsx
+++ src/components/Work/WorkSection.tsx
@@ -4,27 +4,21 @@ import Title from '@/components/Work/Title'
 import DummyProject from '@/components/Work/DummyProject'
 
 const Outer = styled.section`
-  ${tw`h-screen bg-chocolate-400 flex flex-col justify-center align-middle text-center`}
+  ${tw`p-2 pt-64 justify-center align-middle text-center`}
+  min-height: 100vh;
 `
 
 const Inner = styled.div`
-  max-width: 1200px;
   ${tw`w-full self-center`}
-`
-
-const ProjectContainer = styled.div`
-  ${tw`bg-chocolate-500 flex justify-center`}
 `
 
 const WorkContainer: React.FC = () => (
   <Outer id='work'>
     <Inner>
       <Title />
-      <ProjectContainer>
-        {[1, 2, 3].map(el => (
-          <DummyProject key={el} />
-        ))}
-      </ProjectContainer>
+      {[1, 2, 3].map(project => (
+        <DummyProject key={project} />
+      ))}
     </Inner>
   </Outer>
 )
