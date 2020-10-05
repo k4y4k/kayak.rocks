91b195b4a1dab0c0dcfac38b9cd005651943754e
kayak <kayak@kayak.rocks>
2020-10-05 16:58:38 +1000

    feat(hero): redesign hero section

--- src/components/Hero/HeroSection.tsx
+++ src/components/Hero/HeroSection.tsx
@@ -1,23 +1,34 @@
 import React from 'react'
 import tw, { styled } from 'twin.macro'
 import Title from '@/components/Hero/Title'
-import Prologue from '@/components/Hero/Prologue'
-import Epilogue from '@/components/Hero/Epilogue'
-import Button from '@/components/layout/Button'
 import HeroImage from '@/components/Hero/HeroImage'
+import Hi from '@/components/Hero/Hi'
+import Button from '@/components/layout/Button'
 
 const Inner = styled.div`
-  ${tw`w-full self-center p-2 py-4 border-brand border text-white`}
-  max-width: 1200px;
+  ${tw`w-full self-center p-2 py-4 border-brand border text-white bg-cornflowerblue-900 bg-opacity-50 grid`}
+  max-width: 900px;
+  grid-template-columns: 1fr 2fr;
+  grid-template-rows: 1fr 1fr 0.5fr;
+`
+
+const Buttons = styled.div`
+  ${tw`self-end`}
+  grid-row-start: 3;
+  grid-column-start: span 2;
 `
 
 const HeroContainer: React.FC = () => (
   <HeroImage>
     <Inner>
-      <Prologue />
+      <Hi />
       <Title />
-      <Epilogue />
-      <Button>See work</Button>
+
+      <Buttons>
+        <Button>Projects</Button>
+        <Button>GitHub</Button>
+        <Button>LinkedIn</Button>
+      </Buttons>
     </Inner>
   </HeroImage>
 )
