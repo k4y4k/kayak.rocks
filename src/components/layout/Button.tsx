3224e5226ff0fd93a462cea53b479c29c44a51cd
kayak <kayak@kayak.rocks>
2020-10-05 19:43:23 +1000

    feat(buttons): add support for `large` prop
    now buttons can be slightly bigger
    
    closes #65

--- src/components/layout/Button.tsx
+++ src/components/layout/Button.tsx
@@ -6,6 +6,7 @@ import tw, { styled } from 'twin.macro'
 interface ButtonProps {
   className?: string
   important?: boolean
+  large?: boolean
 }
 
 const Button: React.FC<ButtonProps> = ({ children, className }) => (
@@ -18,6 +19,7 @@ const StyledButton = styled(Button)`
   ${tw`uppercase m-2 px-4 py-1 bg-black text-white rounded-lg border border-transparent hover:border-brand hover:text-mediumpurple-500`}
   ${({ important }) =>
     important && tw`bg-tomato-700 hover:text-black hover:border-black`}
+  ${({ large }) => large && tw`text-lg py-2`}
 `
 
 export default StyledButton
