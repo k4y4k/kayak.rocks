743764ba54d2ddc81794f24b781fe7605a1208ce
kayak <kayak@kayak.rocks>
2020-10-05 19:38:50 +1000

    feat(button): add support for `important` prop
    Additionally, add a subtle hover effect
    
    closes #51 and closes #52

--- src/components/layout/Button.tsx
+++ src/components/layout/Button.tsx
@@ -1,23 +1,23 @@
+/* eslint-disable implicit-arrow-linebreak */
+
 import React from 'react'
 import tw, { styled } from 'twin.macro'
 
 interface ButtonProps {
-  children: JSX.Element
   className?: string
+  important?: boolean
 }
 
-const Button: React.FC = ({ children, className }: ButtonProps) => (
+const Button: React.FC<ButtonProps> = ({ children, className }) => (
   <button type='button' className={className}>
     {children}
   </button>
 )
 
 const StyledButton = styled(Button)`
-  ${tw`uppercase m-2 px-4 py-1 bg-black text-white rounded-lg`}
+  ${tw`uppercase m-2 px-4 py-1 bg-black text-white rounded-lg border border-transparent hover:border-brand hover:text-mediumpurple-500`}
+  ${({ important }) =>
+    important && tw`bg-tomato-700 hover:text-black hover:border-black`}
 `
-
-Button.defaultProps = {
-  className: '',
-}
 
 export default StyledButton
