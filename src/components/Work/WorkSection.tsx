f22e9179e90ac71ee3b643a728602fa11a43b56f
kayak <kayak@kayak.rocks>
2020-10-05 10:48:04 +1000

    chore(work): remove Title

--- src/components/Work/WorkSection.tsx
+++ src/components/Work/WorkSection.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import tw, { styled } from 'twin.macro'
-import Title from '@/components/Work/Title'
 import DummyProject from '@/components/Work/DummyProject'
 
 const Outer = styled.section`
-  ${tw`pt-64 justify-center align-middle text-center`}
+  ${tw`justify-center align-middle text-center`}
   min-height: 100vh;
 `
 
@@ -15,7 +14,6 @@ const Inner = styled.div`
 const WorkContainer: React.FC = () => (
   <Outer id='work'>
     <Inner>
-      <Title />
       {[1, 2, 3].map((project, i) => (
         <DummyProject key={project} crisscross={i % 2 === 0} />
       ))}
