5fd8194c4a6ff2daa5583f61e6e5559edca4ed6e
kayak <kayak@kayak.rocks>
2020-10-07 18:17:46 +1000

    feat(projects): add icons / logos to tags
    closes #53

--- src/components/Work/Tag.tsx
+++ src/components/Work/Tag.tsx
@@ -1,18 +1,64 @@
 import React from 'react'
 import tw, { styled } from 'twin.macro'
+import {
+  SiTailwindcss,
+  SiGatsby,
+  SiStripe,
+  SiReact,
+  SiStyledComponents,
+} from 'react-icons/si'
 
 interface TagProps {
   className?: string
   tagText: string
 }
 
-const Tag: React.FC<TagProps> = ({ className, tagText }) => (
-  <span className={className}>{tagText}</span>
-)
+const TagIcon = styled.span`
+  ${tw`text-2xl p-1 pr-2 `}
+`
+
+const Tag: React.FC<TagProps> = ({ className, tagText }) => {
+  let projectIcon
+
+  switch (tagText) {
+    case 'tailwindCSS':
+      projectIcon = <SiTailwindcss />
+      break
+    case 'Gatsby':
+      projectIcon = <SiGatsby />
+      break
+
+    case 'Stripe':
+      projectIcon = <SiStripe />
+      break
+
+    case 'React':
+      projectIcon = <SiReact />
+      break
+
+    case 'styled-components':
+      projectIcon = <SiStyledComponents />
+      break
+
+    default:
+      break
+  }
+
+  return (
+    <div className={className}>
+      <TagIcon>{projectIcon}</TagIcon>
+      <span>{tagText}</span>
+    </div>
+  )
+}
 
 const StyledTag = styled(Tag)`
-  ${tw`p-1 px-2 bg-black text-white m-1 text-sm tracking-widest`}
+  ${tw`p-1 px-2 bg-black text-white m-1 text-sm tracking-widest whitespace-no-wrap`}
   font-family: "Inconsolata", monospace;
+
+  span {
+    ${tw`inline-flex align-middle`}
+  }
 `
 
 export default StyledTag
