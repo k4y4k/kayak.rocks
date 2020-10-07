e5f0ec627953630edd246d246f2f9d0faa09cb5f
kayak <kayak@kayak.rocks>
2020-10-07 20:29:02 +1000

    fix: change navbar's z-index to 40
    closes #90

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -88,7 +88,7 @@ const Navbar: React.FC<NavbarProps> = ({ className }) => {
 }
 
 const StyledNavbar = styled(Navbar)`
-  ${tw`text-white flex p-1 shadow`}
+  ${tw`text-white flex p-1 shadow z-40`}
 `
 
 export default StyledNavbar
