296b759ef9fe28234b34abcedf5bbf66c8d4a7be
kayak <kayak@kayak.rocks>
2020-10-09 15:04:06 +1000

    fix(hero): hero now positions elements responsively
    closes #105

--- src/components/Hero/HeroSection.tsx
+++ src/components/Hero/HeroSection.tsx
@@ -14,7 +14,7 @@ const Inner = styled.div`
   ${tw`w-full self-center p-2 py-4 border-brand border text-white bg-cornflowerblue-900 bg-opacity-50 grid`}
   max-width: 900px;
   grid-template-columns: 1fr 2fr;
-  grid-template-rows: 1fr 1fr 0.5fr;
+  grid-template-rows: 0.5fr 1fr 0.5fr;
 `
 
 const Buttons = styled.div`
