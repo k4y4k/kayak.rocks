e4370274d61e8a96155dd7e4dee56ad61c99057d
kayak <kayak@kayak.rocks>
2021-04-18 15:52:32 +1000

    ✨ (404): new, better 404 page, ft. 😔

--- src/components/Hero/HeroImage.tsx
+++ src/components/Hero/HeroImage.tsx
@@ -6,15 +6,33 @@ import React from 'react'
 interface HeroImageProps {
   className?: string
   children: React.ReactNode
+  bw?: boolean
 }
 
-const HeroImage = ({ className, children }: HeroImageProps): JSX.Element => {
+const HeroImage = ({
+  className,
+  children,
+  bw = false,
+}: HeroImageProps): JSX.Element => {
   const data = useStaticQuery(graphql`
     {
-      file(extension: { eq: "jpg" }, name: { eq: "hero" }) {
+      regular: file(extension: { eq: "jpg" }, name: { eq: "hero" }) {
         id
         childImageSharp {
           fluid(quality: 90, maxWidth: 1200, cropFocus: CENTER) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+
+      bw: file(extension: { eq: "jpg" }, name: { eq: "hero" }) {
+        childImageSharp {
+          fluid(
+            quality: 90
+            grayscale: true
+            maxWidth: 1200
+            cropFocus: CENTER
+          ) {
             ...GatsbyImageSharpFluid_withWebp
           }
         }
@@ -27,7 +45,9 @@ const HeroImage = ({ className, children }: HeroImageProps): JSX.Element => {
       id='home'
       Tag='section'
       className={className}
-      fluid={data.file.childImageSharp.fluid}
+      fluid={
+        bw ? data.bw.childImageSharp.fluid : data.regular.childImageSharp.fluid
+      }
       backgroundColor='#22222'
     >
       {children}
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,52 @@
-import { PageProps } from 'gatsby'
+import tw, { styled } from 'twin.macro'
+import Button from '@/components/layout/Button'
+import HeroImage from '@/components/Hero/HeroImage'
+import { Link } from 'gatsby'
 import React from 'react'
+import SEO from '../seo'
 
-const NotFound: React.FC<PageProps> = () => (
-  <main>
-    <p>Sorry, page not found!</p>
-  </main>
+const Inner = styled.div`
+  ${tw`self-center w-full p-8 py-4 m-8 text-center text-white bg-black border border-brand`}
+  max-width: 900px;
+
+  h1 {
+    ${tw`mb-6 text-6xl`}
+  }
+
+  p {
+    ${tw`text-xl`}
+  }
+`
+
+const Divider = styled.hr`
+  ${tw`w-1/2 mx-auto my-6`}
+`
+
+const HeroContainer = (): JSX.Element => (
+  <>
+    <SEO title='Not found 😢' />
+    <HeroImage bw>
+      <Inner>
+        <h1>😔</h1>
+
+        <p>What you&apos;re looking for isn&apos;t here!</p>
+        <p>
+          And, trust me, the little man that carries the internet over the wires
+          looked really hard.
+        </p>
+
+        <br />
+
+        <p>Try again?</p>
+
+        <Divider />
+
+        <Button important>
+          <Link to='/'> Go home </Link>
+        </Button>
+      </Inner>
+    </HeroImage>
+  </>
 )
 
-export default NotFound
+export default HeroContainer
